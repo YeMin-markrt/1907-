f133a5991a71fd9e4a2dc5a00356cd099a6dc16e
SucessK <1424257169@qq.com>
2020-05-01 20:28:28 +0800

    增加了productInfo页面，和css文件

--- songshu/src/view/ProductInfo.jsx
+++ songshu/src/view/ProductInfo.jsx
@@ -1,11 +1,139 @@
 import React, { Component } from 'react'
-
+import style from "../assets/css/wk/productInfo.module.css"
 export default class ProductInfo extends Component {
   render() {
     return (
-      <div>
-        商品详情
+      <>
+        <div className={style.product_all}>
+          <div className={style.w_header_top}>
+            <span onClick={()=>{this.props.history.go(-1)}} className={" iconfont icon-zuo"} >
+            </span>
+            <p>商品详情</p>
+            <span></span>
+          </div>
+          <div className={style.product_info}>
+            <div className={style.slider_box}>
+              <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/e363b543d4760f44deafbc5a1b9a5873_800x800.jpg" alt=""/>
+              <div></div>
+            </div>
+            <div className={style.info_box}>
+              <p>抖胃味_猪尾香辣味120g</p>
+              <div>
+                <span>22.90 <i>60.00</i></span>
+                <strong>总销量&nbsp;&nbsp;0</strong>
+              </div>
+            </div>
+          </div>
+        </div>
+      {/*  松鼠推荐*/}
+      <div className={style.product_tui}>
+          <p className={style.recommend_title}>松鼠推荐</p>
+        <div className={style.recommendGoods}>
+          <ul className={style.item_cont}>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+            <li>
+              <div className={style.item_img}>
+                <img src="http://pic10.cdn.3songshu.com:81//assets/upload/product/587e6517ebd7e49094045868e2d0a439_380x380.jpg" alt=""/>
+              </div>
+              <p className={style.item_title}>牛奶冻（黑米味）210g</p>
+              <p className={style.item_title_ng}>Q弹mix黑米泡泡酱</p>
+              <p className={style.item_price}><span>4.90</span><i>15.00</i></p>
+            </li>
+          </ul>
+        </div>
       </div>
+          {/*底部导航*/}
+          <div>
+            <ul className={style.footer_bar}>
+              <li>
+                <span className={"iconfont icon-gouwuche"}></span>
+                <p>购物车</p>
+              </li>
+              <li>
+                <span className={"iconfont icon-shoucang"}></span>
+                <p>收藏</p>
+              </li>
+              <li>
+                加入购物车
+              </li>
+              <li>
+                立即购买
+              </li>
+            </ul>
+          </div>
+
+      </>
     )
   }
 }
--- songshu/src/view/ShoppingCart.jsx
+++ songshu/src/view/ShoppingCart.jsx
@@ -10,62 +10,62 @@ export default class ShoppingCart extends Component {
         };
     };
 
-    render() {
+  render() {
 
-        return (
-            <>
-                <div className={"w_header_top"}>
+    return (
+      <>
+        <div className={"w_header_top"}>
             <span className={" iconfont icon-zuo"}>
 
             </span>
-                    <p>购物车</p>
-                    <span>编辑</span>
+            <p>购物车</p>
+            <span>编辑</span>
+        </div>
+        <div className="w_rule">
+            <p>
+                满39元免运费，还差39.00元
+            </p>
+            <p onClick={()=>{this.props.history.push('/carList')}}>
+                去凑单
+                <span  className={"iconfont icon-gantanhao"}></span>
+            </p>
+        </div>
+          <div className={"w_item_common"}>
+                <div>
+                    <img src={no}  alt=""/>
                 </div>
-                <div className="w_rule">
-                    <p>
-                        满39元免运费，还差39.00元
-                    </p>
-                    <p onClick={()=>{this.props.history.push('/carList')}}>
-                        去凑单
-                        <span  className={"iconfont icon-gantanhao"}></span>
-                    </p>
-                </div>
-                <div className={"w_item_common"}>
-                    <div>
-                        <img src={no}  alt=""/>
-                    </div>
-                    <div className={"w_item_content"}>
-                        <div className={"imgbox"}>
-                            <img src={"http://pic10.cdn.3songshu.com:81//assets/upload/product/b4202fe3d5c0929d2d8fa5571913fa60_200x200.jpg"} alt=""/>
-                        </div>
-                        <div className={"w_item2"}>
-                            <p>每日坚果750g/30天装</p>
-                            <p>119.00</p>
-                            <div className={"w_inp"}>
-                                <input className={"button"} type="button" value={"-"}/>
-                                <input type="text" defaultValue= {this.state.num} />
-                                <input className={"button"} type="button" value={"+"}/>
+              <div className={"w_item_content"}>
+                  <div className={"imgbox"}>
+                      <img src={"http://pic10.cdn.3songshu.com:81//assets/upload/product/b4202fe3d5c0929d2d8fa5571913fa60_200x200.jpg"} alt=""/>
+                  </div>
+                  <div className={"w_item2"}>
+                      <p>每日坚果750g/30天装</p>
+                      <p>119.00</p>
+                      <div className={"w_inp"}>
+                          <input className={"button"} type="button" value={"-"}/>
+                          <input type="text" defaultValue= {this.state.num} />
+                          <input className={"button"} type="button" value={"+"}/>
 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className={"w_footer_bar"}>
-                    <div>
-                        <img src={no} alt=""/>
-                        全选
-                    </div>
-                    <div className={"w_foot_right"}>
-                        <div className={"w_foot_total"}>
-                            <p>应付:<strong>0.00</strong>   </p>
-                            <p>总价:<i>0.00</i> <span>优惠:<i>0.00</i></span></p>
-                        </div>
-                    </div>
-                    <div className={"w_foot_pay"}>
-                        去结算
-                    </div>
-                </div>
-            </>
-        )
-    }
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div className={"w_footer_bar"}>
+              <div>
+                  <img src={no} alt=""/>
+                  全选
+              </div>
+              <div className={"w_foot_right"}>
+                  <div className={"w_foot_total"}>
+                      <p>应付:<strong>0.00</strong>   </p>
+                      <p>总价:<i>0.00</i> <span>优惠:<i>0.00</i></span></p>
+                  </div>
+              </div>
+              <div className={"w_foot_pay"}>
+                  去结算
+              </div>
+          </div>
+      </>
+    )
+  }
 }
