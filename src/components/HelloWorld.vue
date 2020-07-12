3e19207b33bfc405ba59249fde6320ae28dc7f0f
yemin <1733327433@qq.com>
2020-07-12 15:23:28 +0800

    测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Button type="primary">hello world</Button>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+      probare, quae sunt a te dicta? Refert tamen, quo modo.
+    </p>
+    <Divider />
+  </div>
+</template>
+
+<script>
+import { Button, Divider } from 'ant-design-vue';
+
+export default {
+    components:{ Button, Divider },
+}
+</script>
